3adfb58d374af014f3d3da3091dc6d9819cffe9d
Angel <angel.moya@correo.unimet.edu.ve>
2025-08-06 21:51:50 -0300

    Add files via upload

--- Departamento.py
+++ Departamento.py
@@ -1,10 +1,10 @@
 class Departamento:
-    def __init__(self, id, nombre, obras):
+    def __init__(self, id, nombre):
         self.id = id
         self.nombre = nombre
-        self.obras = obras
         
+    
     def show(self):
-        print(f"Departamento: {self.nombre}  --  ID: {self.id}") 
+        print(f"Departamento: {self.nombre}  --  ID:({self.id})") 
           
        
